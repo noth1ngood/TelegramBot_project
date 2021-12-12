fc6ac887bc6bf18f8d6b4c84d09e8eecaaa56c72
Андрей <noth1ngood>
2021-12-12 19:03:10 +0300

    3x

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+import datetime
 from config import TOKEN
 bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
@@ -15,7 +16,7 @@ def presettimer(query):
     bot.register_next_step_handler(message, settime)
 def settime(message):
     times = {
-        'cek': 0,
+        'сек': 0,
         'мин': 0,
         'час': 0
     }
@@ -30,8 +31,16 @@ def settime(message):
     times[type_time] = int(quantity)
     presettext(message, times)
 def presettext(message, times):
-    pass
-
+    bot.send_message(message.chat.id,
+                     'Введите текст, который вы хотите получить через данное время')
+    bot.register_next_step_handler(message, settext, times)
+def settext(message, times):
+    cur_date = datetime.datetime.now()
+    izmdate = datetime.timedelta(days=0, hours=times['час'], minutes=times['мин'], seconds=times['сек'])
+    cur_date += izmdate
+    users[message.chat.id] = (cur_date, message.text)
+    bot.send_message(message.chat.id,
+                     'Через заданное время вам придет заданыый текст')
 def get_keyboard():
     keyboard = telebot.types.InlineKeyboardMarkup()
     button = telebot.types.InlineKeyboardButton("Установить таймер", callback_data='set timer')
@@ -39,6 +48,7 @@ def get_keyboard():
     return keyboard
 
 if __name__ == "__main__":
+    users = {}
     while True:
         try:
             bot.polling()
