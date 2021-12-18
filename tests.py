3781b9db4086c9ec928944dda29e4344e15fa40f
Андрей <noth1ngood>
2021-12-18 16:40:15 +0300

    11x with tests

--- tests.py
+++ tests.py
@@ -41,7 +41,18 @@ class TG_test(unittest.TestCase):
             self.assertRegex(m, text)
         except:
             self.assertFalse(True)
-
+    def testurl5(self):
+        try:
+            client.send_message('@He1per_4u_bot', '/books')
+            time.sleep(2)
+            messages = client.get_messages('@He1per_4u_bot')
+            for message in client.get_messages('@He1per_4u_bot', limit=1):
+                m = message.message
+            self.assertEqual(len(messages), 1)
+            text = f'Выберите сайт, на который вы хотите перейти'
+            self.assertRegex(m, text)
+        except:
+            self.assertFalse(True)
     def testurl2(self):
         try:
             client.send_message('@He1per_4u_bot', '/Url')
